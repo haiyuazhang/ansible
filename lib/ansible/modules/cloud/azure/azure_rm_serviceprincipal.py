931f305f6db8241f862b6043db41171e9c1542f5
haiyuazhang <haiyuan.zhang1982@gmaile.com>
2020-06-10 16:59:14 +0800

    first commit, refer to traffic end point implementation

--- lib/ansible/modules/cloud/azure/azure_rm_serviceprincipal.py
+++ lib/ansible/modules/cloud/azure/azure_rm_serviceprincipal.py
@@ -43,89 +43,62 @@ class AzureRMServicePrincipal(AzureRMModuleBaseExt):
     def __init__(self):
 
         self.recourse_group = None
+        self.tenant_id = None
         self.results = dict(changed=False)
         self.state = None
         self.url = None
         self.status_code = [200, 201, 202]
         self.to_do = Actions.NoAction
-        self.mgmt_client = None
-
-        self.body = {}
-        self.query_parameters = {}
-        self.query_parameters['api-version'] = '2019-07-01'
-        self.head_parameters = {}
-        self.header_parameters['Content-Type'] = 'application/json; charset=utf-8'
         self.module_arg_spec=dict()
 
         super(AzureRMServicePrincipal, self).__init__(derived_arg_spec=self.module_arg_spec,
-                                                            supports_check_mode=True,
+                                                            supports_check_mode=False,
                                                             supports_tags=False)
                                             
     def exec_module(self, **kwargs):
         for key in list(self.module_arg_spec.keys()):
-            if hasattr(self, key):
-                setattr(self, key, kwargs[key])
-            elif kwargs[key] is not None:
-                self.body[key] = kwargs[key]
+            setattr(self, key, kwargs[key])
 
-        self.inflate_paramters(self.module_arg_spec, self.body, 0)
-
-        old_response = None
-        response = None
-
-        self.mgmt_client = self.get_mgmt_svc_client(GenericRestClient,
-                                                    base_url=self._cloud_environment.endpoints.resource_manager)
-        
         resource_group = self.get_resource_group(self.resource_group)
+        if not self.location:
+            self.location = resource_group.location
 
-        if 'location' not in self.body:
-            self.body['location'] = resource_group.location
+        to_be_update = False
+        response = self.get_resource()
 
-        old_response = self.get_resource()
-
-        if not old_response:
-            if self.state == 'absent':
-                self.log("Old instance didn't exist")
-            else:
-                self.to_do = Actions.Create
+        if response:
+            if self.state == 'present':
+                to_be_update = self.check_update(response)
+                if to_be_update:
+                    self.action = Actions.CreateOrUpdate
+            elif self.state == 'absent':
+                # delete
+                self.action = Actions.Delete
         else:
-            self.log("Instance already exist")
-            if self.state == 'absent':
-                self.to_do = Actions.Delete
-            elif self.can_update(old_response):
-                self.to_do = Actions.Update
+            if self.state == 'present':
+                self.action = Actions.CreateOrUpdate
+            elif self.state == 'absent':
+                # delete when no exists
+                self.fail("Traffic Manager endpoint {0} not exists.".format(self.name))
 
-        if self.to_do == Actions.NoAction:
-            self.results['changed'] = False
-            response = old_response
-        elif self.to_do == Actions.Delete:
+        if self.action == Actions.CreateOrUpdate:
             self.results['changed'] = True
-            
-            self.delete_resource()
-            while self.get_resource():
-                    time.sleep(20)
-        elif self.to_do == Actions.Create:
-            response = self.create_resource()
+            if self.check_mode:
+                return self.results
+
+            response = self.create_or_update()
+        
+        if self.action == Actions.Delete:
             self.results['changed'] = True
-        else:
-            response = self.update_resource()
-            self.results['changed'] = True
+            if self.check_mode:
+                return self.results
+            response = self.delete_traffic_manager_endpoint()
 
         return self.results
 
     def create_resource(self):
-        self._gen_body()
-        self._gen_url()
-    
         try:
-            response = self.mgmt_client.query(self.url,
-                                              'PUT',
-                                              self.query_parameters,
-                                              self.header_parameters,
-                                              self.body,
-                                              self.status_code,
-                                              600,
-                                              30)
+            client = self.get_graphrbac_client(self.tenant)
         except CloudError as exc:
             self.log('Error attempting to create the GalleryImage instance.')
             self.fail('Error creating the GalleryImage instance: {0}'.format(str(exc)))
@@ -164,28 +137,6 @@ class AzureRMServicePrincipal(AzureRMModuleBaseExt):
             return response
 
         return False
-
-    def can_update(self, old_response):
-        return True
-
-    def _gen_body(self):
-        self.inflate_parameters(self.module_arg_spec, self.body, 0)
-
-    def _gen_url(self):
-        self.url = ('/subscriptions' +
-                    '/{{ subscription_id }}' +
-                    '/resourceGroups' +
-                    '/{{ resource_group }}' +
-                    '/providers' +
-                    '/Microsoft.Compute' +
-                    '/galleries' +
-                    '/{{ gallery_name }}' +
-                    '/images' +
-                    '/{{ image_name }}')
-        self.url = self.url.replace('{{ subscription_id }}', self.subscription_id)
-        self.url = self.url.replace('{{ resource_group }}', self.resource_group)
-        self.url = self.url.replace('{{ gallery_name }}', self.gallery_name)
-        self.url = self.url.replace('{{ image_name }}', self.name)
 
     def _upsert(self):
         try:
